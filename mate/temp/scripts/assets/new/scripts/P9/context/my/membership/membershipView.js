7b9ab6e825690e62ed3ed50938971fa921713ba8
 高健 <732232101@qq.com>
2016-12-15 20:55:46 +0800

    no message

---
+++ mate/temp/scripts/assets/new/scripts/P9/context/my/membership/membershipView.js
@@ -0,0 +1,26 @@
+"use strict";
+cc._RFpush(module, '04170m5NnxHl7w7NwcGne6x', 'membershipView');
+// new/scripts/P9/context/my/membership/membershipView.js
+
+//会籍
+var MVC = require("FWS_MVC");
+var Project = require("Project");
+var membershipView;
+membershipView = cc.Class({
+    "extends": MVC.FMessageConnection,
+
+    properties: {},
+    onEnterNode: function onEnterNode() {
+        //加载结算场景
+        cc.director.loadScene("membershipScene");
+
+        cc.log("loginController onEnterNode");
+        //loadscene。。。
+    },
+    //TODO:当离开此节点的时候会运行这个方法
+    onLeaveNode: function onLeaveNode() {}
+
+});
+module.exports = membershipView;
+
+cc._RFpop();
